a73d22289aa8ec58f0c227b4b7b48027782e1cc9
saad ahmed <saadahmed1742@gmail.com>
2022-12-08 21:42:37 +0500

    updated belt circle

--- src/components/beltcircle.jsx
+++ src/components/beltcircle.jsx
@@ -4,7 +4,7 @@ import belt from "../assets/images/belt.png"
 import beltCircle from "../assets/images/beltCircle.png"
 const Beltcircle = () => {
   return (
-   <div className=' my-[200px] md:px-[50px]'>
+   <div className=' my-[200px]'>
      <div style={{ backgroundImage: `url(${rectangleImage})`}} className="mx-[20px] h-[300px] bg-no-repeat flex justify-between flex-wrap">
         <div className='w-[50%] pl-[30px] py-[30px]'>
             <h1 className='text-[30px] font-bold text-white'>BUY OUR SPECIAL</h1>
@@ -17,11 +17,12 @@ const Beltcircle = () => {
             </div>
         </div>
         <div className='w-[50%]'>
-                <div style={{ backgroundImage: `url(${beltCircle})`}} className="md:h-[455px] md:w-[435px] rounded-[40%]">
-                <div>
-            <div className="md:h-[455px] md:w-[435px]  rounded-[50%] mt-[-105px]" >
+                <div style={{ backgroundImage: `url(${beltCircle})`}} className=" bg-[#2B77C5]  md:h-[355px] md:w-[335px] rounded-[50%]">
+                <div className='flex justify-center items-center h-full'>
+                    <img src={belt} alt="" className='h-[250px]' />
+            {/* <div className="md:h-[455px] md:w-[435px] rounded-[50%] mt-[-105px]" > */}
                     {/* <span>saad</span> */}
-                </div>
+                {/* </div> */}
                 </div>
             </div>
         </div>
